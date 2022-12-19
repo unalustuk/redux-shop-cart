46367f2f1f28958bea4e10cde37c2868d9aa4538
Ünal <unalustk@windowslive.com>
2022-12-19 16:18:37 +0300

    First commit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,37 @@
+import ProductItem from "./ProductItem"
+import classes from "./Products.module.css"
+
+const DUMMY_PRODUCTS = [
+    {
+        id: "p1",
+        price: 6,
+        title: "Book",
+        description: "books",
+    },
+    {
+        id: "p2",
+        price: 3,
+        title: "Banana",
+        description: "Bananas",
+    },
+]
+
+const Products = (props) => {
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map((product) => (
+                    <ProductItem
+                        title={product.title}
+                        id={product.id}
+                        price={product.price}
+                        description={product.description}
+                    />
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+export default Products
